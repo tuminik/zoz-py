ea5e637b4300f7ac82f3207e090eb17c260fe61b
johnnyuran@gmail.com <johnnyuran@gmail.com@4273189d-a538-6910-2021-c7ab68958358>
2014-05-06 04:08:19 +0000

    constantes.py state.py y zozParser.py fueron agregados y ya pueden parsear archivos, y crear estados a partir de parser encontrar espacios en blancos y usarlo como player para calcular una jugada en cada nodo.

---
+++ constantes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+###############################################################################
+#                        Definicion de constantes                             #
+###############################################################################
+
+# Constantes que definen los tipos de movimiento
+MOVE_RIGHT=0
+MOVE_LEFT=1
+MOVE_UP_RIGHT=2
+MOVE_UP_LEFT=3
+MOVE_DOWN_RIGHT=4
+MOVE_DOWN_LEFT=5
+
+# Constantes que definen los caracteres utilizados para representar los elementos del problema
+CHAR_FICHA='#'
+CHAR_SPACE='*'
+CHAR_SPACE_O=' '
+NEWLINE=chr(10)
