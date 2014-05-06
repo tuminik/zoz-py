697c342bbef5fb7b9a6014039d3eb3f1a58bfbad
johnnyuran@gmail.com <johnnyuran@gmail.com@4273189d-a538-6910-2021-c7ab68958358>
2014-05-06 06:55:02 +0000

    probado y funcionando state.py zozParser.py y problem

--- trunk/zozParser.py
+++ trunk/zozParser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import sys
 
-from state import ZozState
+from state import zozState
 from constantes import *
 
 characters = [CHAR_FICHA, 		# Lista de caracteres permitidos dentro de un archivo
@@ -10,9 +10,10 @@ characters = [CHAR_FICHA, 		# Lista de caracteres permitidos dentro de un archiv
               '\n']
 			  
 def imprimir(lista):
-    for i in range(len(lista)):
+    for i in lista:
         #for j in range(len(lista[i])):
-            print lista[i]#[j]
+        print i
+        #print "\n\n"
 			
 def cantidadChar(lista, fila, char):
     cantidad = 0
@@ -20,12 +21,27 @@ def cantidadChar(lista, fila, char):
         cantidad += lista[i].count(char)
         
     return cantidad
+
+def encuadrarLista(lista, columna):
+    for i in range(len(lista)):
+         for j in range(columna - len(lista[i])):
+            lista[i].append(CHAR_SPACE_O)
+    return lista
 			
 def obtenerIndiceLinea(linea, columna, char):
     try:
         return linea.index(char)
     except:
         return columna
+
+def rellenarEspaciosBlancos(columna, linea):
+    linea2 = []
+    linea2 = list(linea)
+    if linea2[len(linea2) - 1] == '\n':
+        linea2[len(linea2) - 1] = CHAR_SPACE_O
+    #for i in range(obtenerIndice(linea, columna)):
+    #    linea2[i] = CHAR_SPACE_O
+    return linea2
 
 def obtieneTamFilCol(columna, fila, linea): #obtiene las dimensiones del mapa
     if len(linea) > columna: #obtiene la columna
@@ -46,12 +62,14 @@ def obtenerMapa(filename, fila, columna):
         for linea in f:
             validaCaracteres(linea)
             fila, columna = obtieneTamFilCol(columna, fila, linea)
-            lista.append(linea) #carga linea por linea el archivo a la lista
+            lista.append(rellenarEspaciosBlancos(columna, linea)) #carga linea por linea el archivo a la lista
     f.closed
     if not cantidadChar(lista, fila, CHAR_SPACE):
         sys.exit("No se encontraron espacios en blanco, Arreglelo!!")
     elif not cantidadChar(lista, fila, CHAR_FICHA):
         sys.exit("No se encontraron Fichas, Arreglelo!!")
+    else:
+        lista = encuadrarLista(lista, columna)
 
     #try
     estado = zozState(lista)
@@ -59,3 +77,14 @@ def obtenerMapa(filename, fila, columna):
     estado.matrixY = columna
 
     return estado
+	
+"""def main():
+    columna = 0
+    fila = 0	
+    initial = obtenerMapa("problem", fila, columna)
+    print initial.matrix
+    print initial.matrixX
+    print initial.matrixY
+	
+if __name__ == "__main__":
+    main()"""
